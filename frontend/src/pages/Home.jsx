72d3786eb29a2416528e1892b342d2e7f8d1fc64
GauntWheelCake <3268356371@qq.com>
2025-02-13 23:26:16 +0100

    testHomepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import LatestCollections from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
+import Title from '../components/Title'
 
 const Home = () => {
     return (
         <div>
+            <Title text1={'I do Some change in develop branch'} text2={'Elmo'} />
             <Hero />
             <LatestCollections />
             <BestSeller />
